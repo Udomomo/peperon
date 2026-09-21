c69d34d9b32bd5553823a4484923574017ee3109
Udomomo <udomomo@gmail.com>
2026-09-21 16:18:35 +0900

    add map to token.ts

--- src/ast/token.ts
+++ src/ast/token.ts
@@ -1,8 +1,8 @@
 export const tokenType = {
   NODE: "NODE", // ノードを表すトークン。
 
-  EDGE_ADD: "->+",
-  EDGE_SUB: "->-",
+  EDGE_ADD: "EDGE_ADD",
+  EDGE_SUB: "EDGE_SUB",
 
   PRIORITY: "PRIORITY", // 優先度を表す
 
@@ -10,11 +10,26 @@ export const tokenType = {
   INPUT: "input",
   OUTPUT: "output",
   HELPER: "node",
+
+  INVALID: "INVALID",
 } as const;
 
+export type TokenType = (typeof tokenType)[keyof typeof tokenType];
+
 export interface Token {
-  type: (typeof tokenType)[keyof typeof tokenType];
+  type: TokenType;
   value: string;
   line: number;
   column: number;
 }
+
+export const keywords: Map<string, TokenType> = new Map([
+  ["input", tokenType.INPUT],
+  ["output", tokenType.OUTPUT],
+  ["node", tokenType.HELPER],
+]);
+
+export const edges: Map<string, TokenType> = new Map([
+  ["->+", tokenType.EDGE_ADD],
+  ["->-", tokenType.EDGE_SUB],
+]);
